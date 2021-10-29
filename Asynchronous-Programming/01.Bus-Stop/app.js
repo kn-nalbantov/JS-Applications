0aa50ace1e53eda55d24bc779edbaf9517af4e6f
kn-nalbantov <kn.nalbantov@gmail.com>
2021-10-29 20:54:43 +0300

    Fixed error handling

--- Asynchronous-Programming/01.Bus-Stop/app.js
+++ Asynchronous-Programming/01.Bus-Stop/app.js
@@ -2,15 +2,20 @@ function getInfo() {
   const stopName = document.getElementById('stopName');
   const listBuses = document.getElementById('buses');
   const input = document.getElementById('stopId');
-  let resObj = {};
 
   fetch(`http://localhost:3030/jsonstore/bus/businfo/${input.value}`)
-    .then(res => res.json())
-    .then(data => {
-      stopName.textContent = data.name;
+    .then(res => {
       while (listBuses.hasChildNodes()) {
         listBuses.firstChild.remove();
       }
+      if (res.status != 200) {
+        stopName.textContent = 'Error';
+        throw new Error('reponse not 200');
+      }
+      return res.json();
+    })
+    .then(data => {
+      stopName.textContent = data.name;
       for (bus in data.buses) {
         const li = document.createElement('li');
         li.textContent = `Bus ${bus} arrives in ${data.buses[bus]} minutes`;
