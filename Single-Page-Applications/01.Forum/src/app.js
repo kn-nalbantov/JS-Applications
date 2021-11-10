7e8720f3ed69bca68238cb492fbffbd28e6c8d91
kn-nalbantov <kn.nalbantov@gmail.com>
2021-11-10 22:28:40 +0200

    Added modules and initialization

---
+++ Single-Page-Applications/01.Forum/src/app.js
@@ -0,0 +1,6 @@
+import { showHome } from './home.js';
+import {showComments} from './comments.js';
+//initialization
+console.log('TODO...');
+
+showHome();
---
+++ Single-Page-Applications/01.Forum/src/home.js
@@ -0,0 +1,10 @@
+//initialization
+//detatchment
+
+const container = document.querySelector('.container');
+const section = document.querySelector('main');
+section.remove();
+
+export function showHome() {
+    container.replaceChildren(section);
+}
