a45962c5a109ae5800a024ad60a84d7f6bec5f78
kn-nalbantov <kn.nalbantov@gmail.com>
2021-11-19 21:20:27 +0200

    Added templating

--- Client-Side-Rendering/03.Search-in-List/search.js
+++ Client-Side-Rendering/03.Search-in-List/search.js
@@ -1,3 +1,13 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+const container = document.getElementById('towns');
 function search() {
-   // TODO
+  const town = data => data.map(t => html`<li>${t}</li>`);
+  const template = html`<ul>
+    ${town(towns)}
+  </ul>`;
+  render(template, container);
 }
+
+search();
--- Client-Side-Rendering/04.Fill-Dropdown/dropdown.js
+++ Client-Side-Rendering/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,53 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+async function populateDropdown() {
+  try {
+    const menu = document.getElementById('menu');
+    const res = await fetch('http://localhost:3030/jsonstore/advanced/dropdown');
+    if (res.status != 200) {
+      throw new Error(res.statusText);
+    }
+    const data = await res.json();
+    const cities = Object.values(data);
+
+    const template = cities.map(c => html`<option value="${c._id}">${c.text}</option>`);
+    render(template, menu);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+populateDropdown();
+
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+async function onSubmit(e) {
+  e.preventDefault();
+
+  try {
+    const text = document.getElementById('itemText');
+    const body = {
+      text: text.value,
+    };
+    if (body.text == '') {
+      throw new Error('You must enter a city name!');
+    }
+    const options = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    };
+    const res = await fetch('http://localhost:3030/jsonstore/advanced/dropdown', options);
+    if (res.status != 200) {
+      throw new Error(res.statusText);
+    }
+    const data = await res.json();
+    populateDropdown();
+    text.value = '';
+  } catch (err) {
+    alert(err.message);
+  }
 }
