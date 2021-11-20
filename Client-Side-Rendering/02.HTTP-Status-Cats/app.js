135945f9cdfcca7980788908962ccded0163d46c
kn-nalbantov <kn.nalbantov@gmail.com>
2021-11-20 18:49:42 +0200

    Added onClick event handler for toggle buttons

--- Client-Side-Rendering/02.HTTP-Status-Cats/app.js
+++ Client-Side-Rendering/02.HTTP-Status-Cats/app.js
@@ -1,25 +1,36 @@
-import {html, render} from '../node_modules/lit-html/lit-html.js';
-import {cats} from './catSeeder.js';
-
-console.log(cats);
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { cats } from './catSeeder.js';
 
 const container = document.getElementById('allCats');
 function loadCats() {
-    
-    const liElements = (data) => data.map(cat => html`
-    <li>
+  const liElements = data =>
+    data.map(
+      cat => html` <li>
         <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap" />
         <div class="info">
-            <button class="showBtn">Show status code</button>
-            <div class="status" style="display: none" id="100">
-                <h4>Status code${cat.statusCode}</h4>
-                <p>Continue</p>
-            </div>
+          <button class="showBtn" @click="${showStatus}">Show status code</button>
+          <div class="status" style="display: none" id="${cat.id}">
+            <h4>Status code${cat.statusCode}</h4>
+            <p>${cat.statusMessage}</p>
+          </div>
         </div>
-    </li>`);
-    const template = html`<ul><li>${liElements(cats)}</li></ul>`;
+      </li>`
+    );
+  const template = html`<ul>
+    ${liElements(cats)}
+  </ul>`;
 
-    render(template, container);
+  render(template, container);
+}
+
+function showStatus(e) {
+  if (e.target.textContent == 'Show status code') {
+    e.target.textContent = 'Hide status code';
+    e.target.parentNode.children[1].style.display = 'block';
+  } else {
+    e.target.textContent = 'Show status code';
+    e.target.parentNode.children[1].style.display = 'none';
+  }
 }
 
 loadCats();
