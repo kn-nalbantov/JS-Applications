1b4d51784803ce96c7d188bf07c43fc7cd27a160
kn-nalbantov <kn.nalbantov@gmail.com>
2021-11-20 19:44:57 +0200

    Finished search functionality and templating

--- Client-Side-Rendering/03.Search-in-List/search.js
+++ Client-Side-Rendering/03.Search-in-List/search.js
@@ -2,8 +2,20 @@ import { html, render } from '../node_modules/lit-html/lit-html.js';
 import { towns } from './towns.js';
 
 const container = document.getElementById('towns');
+const searchText = document.getElementById('searchText');
+const result = document.getElementById('result');
+
+let matches = 0;
+function hasMatched(currentTown) {
+  if (currentTown.toLowerCase().includes(searchText.value.toLowerCase()) && searchText.value != '') {
+    matches++;
+    return true;
+  }
+  return false;
+}
+
 function search() {
-  const town = data => data.map(t => html`<li>${t}</li>`);
+  const town = data => data.map(t => html`<li class="${hasMatched(t) ? 'active' : ''}">${t}</li>`);
   const template = html`<ul>
     ${town(towns)}
   </ul>`;
@@ -11,3 +23,23 @@ function search() {
 }
 
 search();
+
+document.querySelector('button').addEventListener('click', () => {
+  search();
+  const matchTemplate = html`<p>${matches} matches found</p>`;
+  render(matchTemplate, result);
+  matches = 0;
+  searchText.value = '';
+});
+
+
+
+
+
+
+
+
+
+
+
+
