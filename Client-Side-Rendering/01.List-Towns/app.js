ecd26bba1e24541c274de8af3cfb6dc14da118ec
kn-nalbantov <kn.nalbantov@gmail.com>
2021-11-21 09:37:04 +0200

    trimmed input for better UX

--- Client-Side-Rendering/01.List-Towns/app.js
+++ Client-Side-Rendering/01.List-Towns/app.js
@@ -1,13 +1,15 @@
-import {render, html} from '../node_modules/lit-html/lit-html.js';
+import { render, html } from '../node_modules/lit-html/lit-html.js';
 
 const container = document.getElementById('root');
 const form = document.querySelector('form');
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-    e.preventDefault();
-    
-    const towns = form.towns.value.split(', ');
-    const template = html`<ul>${towns.map(t => html`<li>${t}</li>`)}</ul>`;
-    render(template, container);
+  e.preventDefault();
+
+  const towns = form.towns.value.split(',');
+  const template = html`<ul>
+    ${towns.map(t => html`<li>${t.trim()}</li>`)}
+  </ul>`;
+  render(template, container);
 }
