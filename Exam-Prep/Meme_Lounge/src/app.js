d80a75bf976847f6c9d0e2dff1e84d26be492d85
kn-nalbantov <kn.nalbantov@gmail.com>
2021-12-01 21:01:03 +0200

    Updated user navigation, changed some hrefs and added registration, login and logout functionality

--- Exam-Prep/Meme_Lounge/src/app.js
+++ Exam-Prep/Meme_Lounge/src/app.js
@@ -1,21 +1,48 @@
+import { logout } from './api.js';
 import * as api from './data.js';
 import { page, render } from './lib.js';
+import { clearUserData, getUserData } from './util.js';
 import { allMemes } from './views/all-memes.js';
 import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
 
 /* debug */
 window.api = api;
 
 const root = document.querySelector('main');
+document.querySelector('.logoutBtn').addEventListener('click', onLogout);
 
 page(decorateContext);
 page('/', homePage);
 page('/all-memes', allMemes);
+page('/login', loginPage);
+page('/register', registerPage);
 
+updateUserNav();
 page.start();
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
 
     next();
+}
+
+function onLogout() {
+    logout();
+    clearUserData();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+
+    if (userData) {
+        document.querySelector('.user').style.display = 'block';
+        document.querySelector('.guest').style.display = 'none';
+        document.querySelector('.user span').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = 'block';
+    }
 }
---
+++ Exam-Prep/Meme_Lounge/src/views/login.js
@@ -0,0 +1,36 @@
+import { login } from '../api.js';
+import { html } from '../lib.js';
+
+export function loginPage(ctx) {
+  ctx.render(loginTemplate(onSubmit));
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    await login(email, password);
+    ctx.updateUserNav();
+    ctx.page.redirect('/all-memes');
+  }
+}
+
+const loginTemplate = (onSubmit) => html`
+  <section id="login">
+    <form id="login-form" @submit=${onSubmit}>
+      <div class="container">
+        <h1>Login</h1>
+        <label for="email">Email</label>
+        <input id="email" placeholder="Enter Email" name="email" type="text" />
+        <label for="password">Password</label>
+        <input id="password" type="password" placeholder="Enter Password" name="password" />
+        <input type="submit" class="registerbtn button" value="Login" />
+        <div class="container signin">
+          <p>Dont have an account?<a href="/register">Sign up</a>.</p>
+        </div>
+      </div>
+    </form>
+  </section>
+`;
